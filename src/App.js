b9a5736e709624d2d8ae7eb812e0c379857e26fa
ShaikhGaffar <144426220+ShaikhGaffar@users.noreply.github.com>
2024-04-01 11:12:05 +0530

    risponsive

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { PDFDocument } from 'pdf-lib';
+import { FiUpload } from "react-icons/fi";
+import { MdContentPaste } from "react-icons/md";
+
+import { MdOutlinePictureAsPdf } from "react-icons/md";
+
+
 import './App.css';
 
 function DocQueryComponent() {
@@ -9,6 +15,7 @@ function DocQueryComponent() {
     const [resQuestion, setresQuestion] = useState('');
     const [fileName, setFileName] = useState('');
     const [loading, setLoading] = useState(false);
+    const [disable, setdisable] = useState(true);
     const [DataArray, setDataArray] = useState([]);
 
 
@@ -32,7 +39,11 @@ function DocQueryComponent() {
     }
     const handleSubmit = async (e) => {
         e.preventDefault();
+        if (question == '') {
+            return
+        }
         setLoading(true);
+
 
         const formData = new FormData();
         formData.append('file', file);
@@ -45,6 +56,9 @@ function DocQueryComponent() {
 
         const data = await response.json();
         if (data.answer && data.question) {
+            data.timestamp = new Date().getTime();
+            console.log('Timestamp added:', data);
+
             setDataArray((priv) => ([...priv, data]))
             setLoading(false);
             setError('')
@@ -53,6 +67,11 @@ function DocQueryComponent() {
                 top: document.documentElement.scrollHeight - window.innerHeight + 30,
                 behavior: 'smooth' // You can change this to 'auto' for instant scroll
             });
+        } else if (file == null) {
+            setError('Please Upload File')
+            setLoading(false);
+            setQuestion('')
+
         } else {
             setError(data.error)
             setLoading(false);
@@ -60,50 +79,81 @@ function DocQueryComponent() {
 
         }
     };
+    useEffect(() => {
+        if (question) {
+            setdisable(false)
+        }
+    }, [question])
 
     return (
         <div className="container">
             <div className="form-container">
-                <div style={{ marginBottom: "80px", position: "relative" }}>
-                    <div className='stickyhedear' >
-                        <div>
-                            <h1>Upload PDF or JPEG files and get answers to your questions</h1>
-                            <p style={{ fontWeight: "bold", fontSize: '16px' }}>Simply choose a file, enter your question, and click submit to get the answer!</p>
-                        </div>
-                        <div className="file-input-container">
-                            <label className='inputhover' htmlFor="fileInput">Select File</label>
-                            <input className='inputhover' type="file" id="fileInput" onChange={handleFileChange} />
-                        </div>
-                        <div id="fileName"> {fileName ? `File Name : ${fileName} ` : null}</div>
-                    </div>
-                    {
-                        DataArray.length > 0 ?
-                            DataArray.map((item) => {
-                                return (
-                                    <div style={{ display: "flex", justifyContent: "center", marginTop: "10px" }}>
-                                        <div style={{ backgroundColor: "white", width: "70%", padding: "20px", borderRadius: "10px" }}>
-                                            <div className="question-display">{item?.question} </div>
-                                            <div className="answer-display" > <span onClick={() => handleCopyToChailboard(item)} style={{ position: "absolute", right: "10px", top: "-20px", color: "black", fontSize: "14px", cursor: "pointer", }}>Copy</span> <span> {item?.answer} </span></div>
-                                        </div>
-                                    </div>)
-                            }) : null
-                    }
-                </div>
-                <form id="uploadForm" onSubmit={handleSubmit}>
-
-                    {/* <label style={{fontWeight:"bold",fontSize:"16px"}}>Question : </label> */}
-                    <div style={{ display: "flex", justifyContent: "center" }} >
-                        <div style={{ width: "100%", backgroundColor: "black", padding: "10px 0px" }} >
-                            <div >
-                                {Error ?
-                                    <span style={{ color: "red", fontWeight: "400", fontSize: "18px" }}>Error : {Error}</span>
-                                    : null}
+                <div style={{ marginBottom: "50px" }}>
+                    <div style={{ position: "relative" }}>
+                        <div className='stickyhedear' >
+                            {/* <div> */}
+                            {/* <h1>InvoiceQA</h1> */}
+                            {/* <p style={{ fontWeight: "bold", fontSize: '16px' }}>Simply choose a file, enter your question, and click submit to get the answer!</p> */}
+                            {/* </div> */}
+                            <div className="file-input-container">
+                                <label className='inputhover' htmlFor="fileInput"> <span style={{ fontSize: "40px" }}><MdOutlinePictureAsPdf /></span> <span>{fileName ? ` ${fileName} ` : 'Upload File'}</span> </label>
+                                <input className='inputhover' type="file" id="fileInput" onChange={handleFileChange} />
                             </div>
-                            <input className='input' type="text" value={question} onChange={(e) => setQuestion(e.target.value)} placeholder="Enter your question" />
-                            <button disabled={loading} type="submit " className='submit'><div style={{ display: "flex", alignItems: 'center', gap: '10px' }}> {loading ? 'Submit' : "Submit"} <div className="loader" style={{ display: loading ? 'block' : 'none' }}></div> </div></button>
+
                         </div>
+                        {
+                            DataArray.length > 0 ?
+                                DataArray.map((item) => {
+                                    return (
+                                        <div style={{ display: "flex", justifyContent: "center", marginTop: "10px", }}>
+                                            <div className='hoveriffect answerArea' >
+                                                <div style={{ display: "flex", flexDirection: "column" }}>
+                                                    <span style={{ textAlign: "left", margin: "10px 0px", color: "#6e6b6b", position: "relative" }}>
+                                                        <span> Question </span>  <span className='activeOnhover' style={{ position: "absolute", right: "0px", top: "0px", fontSize: "13px" }} >  {item?.timestamp ? new Date(item.timestamp).toLocaleString() : null} </span>
+                                                    </span>
+                                                    <div className="question-display">{item?.question} </div>
+                                                </div>
+                                                <div style={{ display: "flex", flexDirection: "column" }} className="answer-display" >
+                                                    <span style={{ margin: "10px 0px 10px -10px", color: "#6e6b6b", position: "relative" }}>
+                                                        <span> Answer </span>  <span className='activeOnhover' style={{ position: "absolute", right: "0px", top: "0px", fontSize: "13px" }} onClick={() => handleCopyToChailboard(item)}> <MdContentPaste /> Copy </span>
+                                                    </span>
+
+                                                    {/* <span onClick={() => handleCopyToChailboard(item)} style={{ position: "absolute", right: "10px", top: "-20px", color: "black", fontSize: "14px", cursor: "pointer", }}>Copy</span>  */}
+                                                    <div style={{ fontWeight: "bold", padding: '10px 10px 10px 0px' }}> {item?.answer} </div></div>
+                                            </div>
+                                        </div>)
+                                }) : (
+                                    <div style={{ display: "flex", justifyContent: "center", marginTop: "10px" }}>
+                                        <div className='headers'>
+                                            <div>
+                                                <h1>InvoiceQA</h1>
+                                                <p>
+                                                    Simply choose a file, enter your question, and click submit to get the answer!
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                        }
                     </div>
-                </form>
+                    <form id="uploadForm" onSubmit={handleSubmit}>
+
+                        {/* <label style={{fontWeight:"bold",fontSize:"16px"}}>Question : </label> */}
+                        <div style={{ display: "flex", justifyContent: "center" }} >
+                            <div className='questionArea' >
+                                <div >
+                                    {Error ?
+                                        <span style={{ color: "red", fontWeight: "400", fontSize: "18px" }}>Error : {Error}</span>
+                                        : null}
+                                </div>
+                                <div>
+                                    <input className='input' type="text" value={question} onChange={(e) => setQuestion(e.target.value)} placeholder="Enter your question" />
+                                    <button disabled={loading || disable} type="submit " className='submit'><div style={{ display: "flex", alignItems: 'center', gap: '10px' }}> {loading ? 'Submit' : "Submit"} <div className="loader" style={{ display: loading ? 'block' : 'none' }}></div> </div></button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     );
